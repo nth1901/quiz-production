245daccc4d6a733d534facba5f6b076d058b08c6
nth1901 <nth19193@gmail.com>
2025-07-12 21:09:11 +0700

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; 
 
 // Import các route module
 const quizRoutes = require('./routes/quiz');
